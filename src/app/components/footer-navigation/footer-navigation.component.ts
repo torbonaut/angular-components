2da82dd929b16b2edb1d1bc94909ea07d951f13d
Torben Gallob <office@lalit.at>
2020-01-05 02:42:06 +0100

    footer nav

--- src/app/components/footer-navigation/footer-navigation.component.ts
+++ src/app/components/footer-navigation/footer-navigation.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, HostBinding} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {DomSanitizer} from '@angular/platform-browser';
+
+export interface IMenuItem {
+  title: string;
+  iconURL: string;
+  link?: string;
+}
 
 @Component({
   selector: 'app-footer-navigation',
@@ -8,9 +16,28 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class FooterNavigationComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('attr.style')
+  public get valueAsStyle(): any {
+    return this.sanitizer.bypassSecurityTrustStyle(`--active: ${this.selected$.getValue()}`);
+  }
+
+  menu: IMenuItem[] = [
+    { title: 'Home', iconURL: '/assets/home-alt-regular.svg' },
+    { title: 'Profile', iconURL: '/assets/user-light.svg' },
+    { title: 'Settings', iconURL: '/assets/cog-light.svg' }
+  ];
+
+  selected$: BehaviorSubject<number> = new BehaviorSubject(0);
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
 
+  select(li: HTMLLIElement, index: number) {
+    this.selected$.next(index);
+    li.style.setProperty('--i', String(index));
+  }
+
 }
+
